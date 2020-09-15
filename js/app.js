2e549fc964216d325b07e865cf9afa6953c39959
Maxime Raynal <me@maxproject.fr>
2020-09-15 10:48:15 +0200

    Etape 2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
+function onClickOnPixel(event) {
+
+    // Etape 2.2 : Une fois le clic fait sur la cellule je veux récupérer la cellule cliqué
+    var cell = event.target;
+
+
+    // Etape 2.3 : Sur cette cellule, s'il n'y a rien je met la classe bg-black
+    //             s'il y a la classe je l'enlève
+    //
+    cell.classList.toggle('bg-black');
+
+    // classList.toggle correspon au comportement suivant :
+    // if (cell.classList.contains('bg-black')) {
+    //     cell.classList.remove('bg-black');
+    // } else {
+    //     cell.classList.add('bg-black');
+    // }
+}
+
 // Objectif :
-// Créer un tableau de 8x8 cases
+// Etape 1 - Créer un tableau de 8x8 cases
+// Etape 2 - Réagir au clic sur une cellule
 
 // Étape 1.1 - Récupérer la "target"
 
@@ -17,4 +37,7 @@ for (var indexCell = 0; indexCell < nbCellules; indexCell++) {
     var cell = document.createElement('div');
     cell.classList.add('pixel');
     gameBoard.appendChild(cell);
+
+    // Etape 2.1 : Je veux réagir au clic sur chaque cellule
+    cell.addEventListener('click', onClickOnPixel);
 }
